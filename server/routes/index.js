bfb7998d3f4c1ba8c8f0ca65e10aa1bf7834b974
Evan <evan3826t@gmail.com>
2022-09-24 15:37:12 +0800

    init finish frontend: Vue backend: express

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,12 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/personal', function(req,res,next){
+router.get('/personal', function (req, res, next) {
   res.send(req.query)
 })
 
-router.post('/favorite', function(req, res, next){
+router.post('/favorite', function (req, res, next) {
   res.send(req.body)
 })
 
-module.exports = router;
+module.exports = router
