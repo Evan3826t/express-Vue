3ade9891d5b22b9d8979f294bac7aec3e7d11119
Evan <evan3826t@gmail.com>
2022-09-25 18:02:32 +0800

    add about page & use bootstrap vue

---
+++ src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      msg: 'about'
+    }
+  },
+  methods: {
+    // getmsg () {
+    //   this.$axios.get('/api/personal?name=MUKI').then(function (res) {
+    //     console.log('res=', res)
+    //   })
+    // },
+    // postmsg () {
+    //   this.$axios.post('/api/favorite', {name: 'money'}).then(res => {
+    //     console.log('post res = ', res)
+    //   })
+    // }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import about from '@/components/About'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +13,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/about',
+      name: 'about',
+      component: about
     }
   ]
 })
