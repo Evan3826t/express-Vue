eb925d5109d8cda1b567afb20a9d3943effe2f7c
谷啟祥 <evan.gu@piapp.com.tw>
2022-09-28 17:22:31 +0800

    dockerize vue & express

--- config/index.js
+++ config/index.js
@@ -12,14 +12,14 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: '0.0.0.0:3000',
         ChangeOrigin: true
       }
     },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 9002, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
